e2b28d3603629e44b7dddb6c34b501ff3af63abc
Sverre <79341182+warm-molerat@users.noreply.github.com>
2023-11-23 11:50:04 +0000

    Saved and then re-committed the file.

--- commit1_Github_2023_11_23.R
+++ commit1_Github_2023_11_23.R
@@ -1,1 +1,9 @@
 #commit this
+
+#I'm making some more changes.
+#I am not sure how tonow to commit the new changes, as they don't show
+#up in the git-section... 
+
+#Okay, after saving it, it does. 
+#I have no clue why the same doesn't happen to "github_2023_11_23" when I 
+#do the same thing... 
