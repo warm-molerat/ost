0978fc708c42d850d2ce8713863b7c18900230c9
Sverre <79341182+warm-molerat@users.noreply.github.com>
2023-11-23 12:51:00 +0000

    Created a script to plot penguin size by species

---
+++ penguins.R
@@ -0,0 +1,10 @@
+library(palmerpenguins)
+
+
+names(penguins)
+
+ggplot(penguins, aes(species, body_mass_g)) +
+  geom_boxplot() +
+  labs(y = "Body mass [g]",
+       x = "Species") +
+  theme_classic()
